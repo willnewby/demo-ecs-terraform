8f0c65336494ac47e08fcca00e4544621bffc152
Will Newby <willnewby@gmail.com>
2017-04-03 14:43:19 -0600

    Adding IAM Policy/Role/Instance config since ecsInstanceRole isn't a default

--- iam_policies.tf
+++ iam_policies.tf
@@ -17,3 +17,22 @@ resource "aws_iam_role_policy" "ecs_service_role_policy" {
   policy   = "${data.template_file.ecs_service_role_policy.rendered}"
   role     = "${aws_iam_role.ecs_role.id}"
 }
+
+data "template_file" "ecs_instance_role_policy" {
+    template = "${file("iam_policy_docs/ecs-instance-role-policy.json")}"
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "helloEcsInstanceRole"
+  assume_role_policy = "${data.template_file.ecs_instance_role_policy.rendered}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+    role       = "${aws_iam_role.ecs_instance_role.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name  = "test_profile"
+  roles = ["${aws_iam_role.ecs_instance_role.name}"]
+}
